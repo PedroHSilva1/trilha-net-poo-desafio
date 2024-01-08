959cb8248085146153155e0e7d283a4358f9aaea
PedroHSilva1 <148078634+PedroHSilva1@users.noreply.github.com>
2024-01-08 18:27:16 -0300

    Add files via upload

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,24 +1,102 @@
+using System.Diagnostics;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo {get; set;}
+        protected string Imei {get;set;}
+        protected int Memoria {get;set;}
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            this.Modelo = modelo;
+            this.Imei = imei;
+            this.Memoria = memoria;
+        }
+
+        public void Config()
+        {
+            Console.Clear();
+            Console.WriteLine("Acessando as confiurações.");
+            Thread.Sleep(800);
+            Console.Clear();
+            Console.WriteLine("Acessando as confiurações..");
+            Thread.Sleep(800);
+            Console.Clear();
+            Console.WriteLine("Acessando as confiurações...");
+            Thread.Sleep(800);
+
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Emei: {Imei}");
+            Console.WriteLine($"Memoria: {Memoria}");
+            Console.WriteLine($"Número: {Numero}");
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.Clear();
+            Console.WriteLine("Selecione um contato para ligar:");
+            Console.WriteLine("\n1. Mãe\n2. Pai\n3. Irmão");
+            int selecao = Int32.Parse(Console.ReadLine());
+
+            switch (selecao)
+            {
+                case 1:
+                Console.Clear();
+                Console.WriteLine("Ligando para 'Mãe'...");
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.WriteLine("Chamada perdida...");
+                break;
+
+                case 2:
+                Console.Clear();
+                Console.WriteLine("Ligando para 'Pai'...");
+                Thread.Sleep(1000);
+                Console.WriteLine("O número que você ligou está fora de area ou desligado.");
+                break;
+                
+                case 3:
+                Console.Clear();
+                Console.WriteLine("Ligando para 'Irmão'...");
+                Thread.Sleep(1000);
+                Console.WriteLine("O número que você ligou está fora de area ou não existe.");
+                break;
+
+                default:
+                Ligar();
+                break;
+            }
         }
 
         public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
+        {   
+            Console.Clear(); 
+            Console.WriteLine("Recebendo ligação...\nPossível fraude.\nAtender? (S/N)");
+            string escolha = Console.ReadLine();
+
+            if (escolha == "s" || escolha == "S")
+            {
+                Console.Clear();
+                Console.WriteLine("\nAlo? Aqui é do Banco do Brasil...");
+            }
+            else if (escolha == "n" || escolha == "N")
+            {
+                Console.Clear();
+                Console.WriteLine("\nChamada recusada!");
+            }
+            else
+            {
+                Console.WriteLine("\nComando desconhecido.\n");
+                ReceberLigacao();
+            }
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,99 @@
-﻿using DesafioPOO.Models;
+﻿using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO.Models
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("Bem-vindo! Escolha um celular:");
+            Console.WriteLine("1. iPhone");
+            Console.WriteLine("2. Nokia");
+
+            int escolhaCelular = LerInteiro("Escolha o número do celular: ");
+
+            Smartphone celular;
+
+            switch (escolhaCelular)
+            {
+                case 1:
+                    celular = new Iphone("123456789", "iPhone X", "123456789012345", 64);
+                    break;
+                case 2:
+                    celular = new Nokia("987654321", "Nokia 3310", "987654321098765", 16);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Saindo do programa.");
+                    return;
+            }
+
+            ExibirMenuCelular(celular);
+        }
+
+        static void ExibirMenuCelular(Smartphone celular)
+        {
+            bool sair = false;
+
+            while (!sair)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu do Celular:");
+                Console.WriteLine("1. Configurações");
+                Console.WriteLine("2. Fazer Ligação");
+                Console.WriteLine("3. Receber Ligação");
+                Console.WriteLine("4. Instalar Aplicativo");
+                Console.WriteLine("5. Mudar número telefonico");
+                Console.WriteLine("6. Sair.");
+
+                int escolha = LerInteiro("Escolha uma opção: ");
+
+                switch (escolha)
+                {
+                    case 1:
+                        celular.Config();
+                        break;
+                    case 2:
+                        celular.Ligar();
+                        break;
+                    case 3:
+                        celular.ReceberLigacao();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.Write("Digite o nome do aplicativo a ser instalado: ");
+                        string nomeApp = Console.ReadLine();
+                        celular.InstalarAplicativo(nomeApp);
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Console.Write("Digite o novo número do celular: ");
+                        string novoNumero = Console.ReadLine();
+                        celular.Numero = novoNumero;
+                        Console.WriteLine("Número do celular alterado com sucesso!");
+                        break;
+                    case 6:
+                        sair = true;
+                        Console.WriteLine("Saindo do programa. Até logo!");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+                    Console.WriteLine("\nPressione uma tecla para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+            }
+        }
+
+        static int LerInteiro(string prompt)
+        {
+            int valor;
+            do
+            {
+                Console.Write(prompt);
+            } while (!int.TryParse(Console.ReadLine(), out valor));
+            return valor;
+        }
+    }
+}
